f3f405364e645d69f029ff693062c2b80b46d097
Peter Schultz <peter.schultz@classmarkets.com>
2014-11-06 16:58:22 +0100

    cleanup cache directories and run integration with debug on and off

--- Tests/Integration/IntegrationTest.php
+++ Tests/Integration/IntegrationTest.php
@@ -3,23 +3,36 @@
 namespace Classmarkets\RavenBundle\Tests\Integration;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Yaml\Yaml;
 
-class IntegrationTest extends WebTestCase
+class IntegrationTest extends \PHPUnit_Framework_TestCase
 {
-    protected static function getKernelClass()
-    {
-        return 'Classmarkets\RavenBundle\Tests\Integration\TestKernel';
-    }
-
-    public function testErrorPage()
+    /** @dataProvider getConfigurations */
+    public function testErrorPage($config, $debug)
     {
         if (defined('HHVM_VERSION')) {
             $this->markTestSkipped("Skipping on hhvm (xml parsing in the framework bundle fails");
         }
 
-        $client = static::createClient();
+        $env = uniqid(); // avoid re-declaring classes in the container dump
+        $kernel = new TestKernel($env, $debug, $config);
+        $kernel->boot();
+        $client = $kernel->getContainer()->get('test.client');
+
         $client->request('GET', '/_cm_raven_bundle_test');
 
         $this->assertEquals("Error Code: abc\n", $client->getResponse()->getContent());
+    }
+
+    public function getConfigurations()
+    {
+        $configurations = array();
+
+        foreach (glob(__DIR__ . '/configs/test*.yml') as $configFilename) {
+            $configurations[] = array($configFilename, $debug = true);
+            $configurations[] = array($configFilename, $debug = false);
+        }
+
+        return $configurations;
     }
 }
--- Tests/Integration/TestKernel.php
+++ Tests/Integration/TestKernel.php
@@ -3,10 +3,28 @@
 namespace Classmarkets\RavenBundle\Tests\Integration;
 
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\Kernel;
 
 class TestKernel extends Kernel
 {
+    private $configFilename;
+
+    public function __construct($env, $debug, $configFilename)
+    {
+        parent::__construct($env, $debug);
+
+        $this->configFilename = $configFilename;
+
+        $this->cacheDir = tempnam(sys_get_temp_dir(), 'raven-bundle-test');
+        unlink($this->cacheDir);
+
+        register_shutdown_function(function () {
+            $fs = new Filesystem();
+            $fs->remove($this->cacheDir);
+        });
+    }
+
     public function registerBundles()
     {
         return array(
@@ -18,8 +36,13 @@ class TestKernel extends Kernel
         );
     }
 
+    public function getCacheDir()
+    {
+        return $this->cacheDir;
+    }
+
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config.yml');
+        $loader->load($this->configFilename);
     }
 }
