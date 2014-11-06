909006acc9b2de3157538932e22dc85978b3d39a
Peter Schultz <peter.schultz@classmarkets.com>
2014-11-06 09:47:21 +0100

    don't store exceptions themselves to save memory

--- SentryEventRecorder.php
+++ SentryEventRecorder.php
@@ -4,25 +4,31 @@ namespace Classmarkets\RavenBundle;
 
 class SentryEventRecorder
 {
-    /** @var \SplObjectStorage */
+    /** @var string[] */
     private $idStore;
 
     public function __construct()
     {
-        $this->idStore = new \SplObjectStorage();
+        $this->idStore = array();
     }
 
     public function addExceptionEventId($exception, $id)
     {
-        if (is_object($exception)) {
-            $this->idStore->attach($exception, $id);
+        if (is_object($exception) && !empty($id)) {
+            $this->idStore[spl_object_hash($exception)] = $id;
         }
     }
 
     public function getEventIdForException($exception)
     {
-        if (is_object($exception) && $this->idStore->contains($exception)) {
-            return $this->idStore[$exception];
+        if (!is_object($exception)) {
+            return null;
+        }
+
+        $hash = spl_object_hash($exception);
+
+        if (isset($this->idStore[$hash])) {
+            return $this->idStore[$hash];
         }
 
         return null;
